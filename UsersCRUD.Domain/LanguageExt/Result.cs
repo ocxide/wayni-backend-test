88bd993a147cb442c5c2a825d858363ecfb660a6
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-09-19 19:53:47 -0500

    refactor: create Result type

---
+++ UsersCRUD.Domain/LanguageExt/Result.cs
@@ -0,0 +1,29 @@
+namespace UsersCRUD.Domain.LanguageExt;
+
+public abstract class Result<T, E>
+{
+    public static Result<T, E> Ok(T value) => new Ok<T, E> { Value = value };
+
+    public static Result<T, E> Err(E err) => new Err<T, E> { Error = err };
+
+    public U Match<U>(Func<T, U> ok, Func<E, U> err)
+    {
+        if (this is Ok<T, E> okVal)
+        {
+            return ok(okVal.Value);
+        }
+
+				var errVal = (this as Err<T, E>)!.Error;
+				return err(errVal);
+    }
+}
+
+public sealed class Ok<T, E> : Result<T, E>
+{
+    public required T Value { get; init; }
+}
+
+public sealed class Err<T, E> : Result<T, E>
+{
+    public required E Error { get; init; }
+}
